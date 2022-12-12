00d8f08e7f29f07147f266571a59b266b0623a00
CKabapod <vanyabrox2018@gmail.com>
2022-12-12 22:32:23 +0200

    comments for me

--- counting.py
+++ counting.py
@@ -6,32 +6,37 @@ class Main:
         self.file_name = file_name
         self.inputs = []
         self.object = {}
-        
+
     def getJSON(self):
+        # ! Placing block's stats in a dict
         try:
-            with open ('blocks/' + self.file_name + '.json') as file:
+            with open('blocks/' + self.file_name + '.json') as file:
                 self.object = json.load(file)
             print('Take block passed.')
             return self.object
         except Exception as e:
             print(f"Error: {e}")
-            
+
     def getInput(self):
+        # * Taking object
         object = self.getJSON()
-        
+
+        # ? If 3 input exists
         if hasattr(object, 'input3'):
             self.inputs = [object['input1'] / object['input1_time'],
                            object['input2'] / object['input2_time'],
                            object['input3'] / object['input3_time']]
-            print("Inputs passed")
+            print("Inputs confirmed.")
             return self.inputs[0] + self.inputs[1] + self.inputs[2]
         else:
             self.inputs = [object['input1'] / object['input1_time'],
                            object['input2'] / object['input2_time']]
-            print("Inputs passed")
+            print("Inputs confirmed.")
             return self.inputs[0] + self.inputs[1]
 
     def getResult(self, input):
+        # * Taking object
         object = self.object
+        # ! Calculating output
         output = object['prod_time'] + object['energy'] / object['size']
         return round(input + output, 2)
--- single.py
+++ single.py
@@ -4,10 +4,12 @@ import counting as count
 class Main:
     def __init__(self):
         self.object_name = "small_sili"
+        # ! Creating a "counting" object through the library
         self.object = count.Main(self.object_name)
 
-if __name__ == "__main__":
-    cs = count.Main(Main().object_name)
-    input = cs.getInput()
 
+if __name__ == "__main__":
+    cs = Main().object
+    # * Taking an input from the object
+    input = cs.getInput()
     print(f"Block's efficiency is {cs.getResult(input)}")
